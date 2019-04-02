776f9b1b353def69fd714566af6ba0652de25ab0
Luis Bandarra <laruibasar@users.noreply.github.com>
2019-04-02 21:59:45 +0100

    start dev ex3

--- se1/ex3.c
+++ se1/ex3.c
@@ -6,8 +6,48 @@
  */
 #include <stdio.h>
 
+/* union type to store value and interchange  value */
+typedef union {
+    int inum,
+    float fnum
+} Float;
+
+int
+string_int(char *string, long *number)
+{
+    int exp = 0;
+    int decimal = 0;
+    long *number = 0;
+    char c;
+    
+    while (c = string[exp] != 0) {
+        if (c == ',' || c == '.') {
+            decimal = 1;
+            continue;
+        }
+        
+        *number = (*number * 10) + (c - '0');
+        if (decimal) {+
+            exp++;
+        }
+    }
+    
+    return exp;
+}
+
 float
-string_to_float(char *string);
+string_to_float(char *string)
+{
+    // convert string to int
+    long *number;
+    int exp = string_int(string, number);
+    
+    // shift int to bin for validate
+    
+    // convert bin to signal, expoent, mantissa
+ 
+    // save    
+}    
 
 int
 main(int argc, char *argv[])
